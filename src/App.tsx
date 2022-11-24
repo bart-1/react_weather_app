3b2241a10764c0a4c7c64f278e45f4757cf2d54f
Bartosz Woldanski <studio@dziwnykot.pl>
2022-11-24 23:52:51 +0100

    feat: convert api data refactor: delete unused code refactor: api key in separated file

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,8 @@
 import { useEffect, useState } from "react";
 import { filterBlocks, filterText, isIterable } from "./helpers";
-
-
+import { apiKey } from "../apiKey";
 
 function App() {
-  const [url, setUrl] = useState(
-    "https://api.openweathermap.org/data/2.5/weather?q=warszawa&units=metric&APPID=d923dc3ed6c037a2d2997ef6466fb01f"
-  );
   const [dataArray, setDataArray] = useState({});
 
   const iterateIt = (dataPcs: any, masterKey = "") => {
@@ -23,49 +19,60 @@ function App() {
   const apiFetch = async (url: string) => {
     const response = await fetch(url)
       .then((response) => response.json())
-      .catch((err) => console.log(err));
+      .then((response) => {
+        setDataArray({});
+        iterateIt(response);
+      })
 
-    setDataArray([]);
-    iterateIt(response);
+      .catch((err) => console.log(err));
   };
 
   useEffect(() => {
-    apiFetch(url);
-    const mainTimer = setInterval(() => apiFetch(url), 50000);
+    const apiData = apiFetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=warszawa&units=metric&APPID=${apiKey}`
+    );
+
+    const mainTimer = setInterval(() => {
+      const apiData = apiFetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=warszawa&units=metric&APPID=${apiKey}`
+      );
+    }, 50000);
 
     return () => {
       clearInterval(mainTimer);
     };
   }, []);
 
-
+  if (Object.keys(dataArray).length === 0)
+    return <span>Data is loading...</span>;
 
   const blocks = Object.entries(dataArray).map(([key, value], index) => {
-    const block = filterBlocks(key);
+    const block = filterBlocks(key, value);
 
     return block.show && block ? (
       <div
-        className="flex flex-col border-2 border-blue-900 bg-gradient-to-b from-gray-900 to-transparent rounded-xl hover:scale-150"
+        className={`flex flex-col border-2 border-blue-900 bg-gradient-to-b from-gray-900 to-transparent rounded-xl hover:scale-150 ${block.className}`}
         key={index}>
         <span className="text-xs p-3 text-white bg-black rounded-t-xl">
           {block.title ? block.title : filterText(String(key))}
         </span>
+        {block.icon ? block.icon : null}
         <span className="text-red-500 p-3 text-2xl font-bold">
-          {`${value} ${block.unit}`}
+          {block.processedData
+            ? `${block.processedData} ${block.unit}`
+            : `${value} ${block.unit}`}
         </span>
       </div>
-    ) : (
-      null
-    );
+    ) : null;
   });
 
   if (!blocks) return <div>Epmty</div>;
   // );
   return (
     <div className="App m-0 p-0 box-border">
-        <div className="mt-12 max-w-2xl flex flex-wrap gap-3 m-auto justify-center">
-          {blocks}
-        </div>
+      <div className="mt-12 max-w-2xl flex flex-wrap gap-3 m-auto justify-center">
+        {blocks}
+      </div>
     </div>
   );
 }
--- src/helpers.tsx
+++ src/helpers.tsx
@@ -1,79 +1,267 @@
+const transformDate = (date: number) => {
+  const convDate = new Date(date * 1000);
+  const seconds = "0" + convDate.getSeconds();
+  const minutes = "0" + convDate.getMinutes();
+  const hours = "0" + convDate.getHours();
 
+  return `${
+    hours.slice(-2) + ":" + minutes.slice(-2) + ":" + seconds.slice(-2)
+  }`;
+};
 
 export function filterText(text: string) {
   return text.replaceAll("_", " ");
 }
 
-export function filterBlocks(text: string) {
+export function filterBlocks(text: string, value: string | number | unknown) {
   switch (true) {
     case text === "coord_lon":
-      return { show: false, title: "", unit: "", iconShow: true, icon: <></> };
+      return {
+        show: false,
+        title: "",
+        unit: "",
+        processedData: "",
+        icon: null,
+        className: ""
+      };
     case text === "coord_lat":
-      return { show: false, title: "", unit: "", iconShow: true, icon: <></> };
+      return {
+        show: false,
+        title: "",
+        unit: "",
+        processedData: "",
+        icon: null,
+        className: ""
+      };
     case text === "0_id":
-      return { show: false, title: "", unit: "", iconShow: true, icon: <></> };
+      return {
+        show: false,
+        title: "",
+        unit: "",
+        processedData: "",
+        icon: null,
+        className: ""
+      };
     case text === "0_main":
-      return { show: true, title: "Icon", unit: "", iconShow: true, icon: <></> };
+      return {
+        show: false,
+        title: "",
+        unit: "",
+        processedData: "",
+        icon: null,
+        className: ""
+      };
     case text === "0_description":
-      return { show: true, title: "Description", unit: "", iconShow: true, icon: <></> };
+      return {
+        show: true,
+        title: "Description",
+        unit: "",
+        processedData: "",
+        icon: null,
+        className: ""
+      };
     case text === "0_icon":
-      return { show: true, title: "Icon", unit: "", iconShow: true, icon: <></> };
+      return {
+        show: true,
+        title: ` `,
+        unit: "",
+        processedData: " ",
+        icon: (
+          <img src={`http://openweathermap.org/img/wn/${value}@2x.png`}></img>
+        ),
+        className: "order-1",
+      };
     case text === "base":
-      return { show: false, title: " ", unit: "", iconShow: true, icon: <></> };
+      return {
+        show: false,
+        title: " ",
+        unit: "",
+        processedData: "",
+        icon: null,
+        className: ""
+      };
     case text === "main_temp":
       return {
         show: true,
         title: "Temperature",
         unit: "\u{2103}",
-        iconShow: true,
-        icon: <></>,
+        processedData: `${Math.round(Number(value))}`,
+        icon: null,
+        className: ""
       };
     case text === "main_feels_like":
-      return { show: true, title: "Feels like", unit: "\u{2103}", iconShow: true, icon: <></> };
+      return {
+        show: true,
+        title: "Feels like",
+        unit: "\u{2103}",
+        processedData: `${Math.round(Number(value))}`,
+        icon: null,
+        className: ""
+      };
     case text === "main_temp_min":
-      return { show: true, title: "Todays min", unit: "\u{2103}", iconShow: true, icon: <></> };
+      return {
+        show: true,
+        title: "Todays min",
+        unit: "\u{2103}",
+        processedData: `${Math.round(Number(value))}`,
+        icon: null,
+        className: ""
+      };
     case text === "main_temp_max":
-      return { show: true, title: "Todays max", unit: "\u{2103}", iconShow: true, icon: <></> };
+      return {
+        show: true,
+        title: "Todays max",
+        unit: "\u{2103}",
+        processedData: `${Math.round(Number(value))}`,
+        icon: null,
+        className: ""
+      };
     case text === "main_pressure":
-      return { show: true, title: "Pressure", unit: "hPa", iconShow: true, icon: <></> };
+      return {
+        show: true,
+        title: "Pressure",
+        unit: "hPa",
+        processedData: "",
+        icon: null,
+        className: ""
+      };
     case text === "main_humidity":
       return {
         show: true,
         title: "Humidity",
         unit: "\u{0025}",
-        iconShow: true,
-        icon: <></>,
+        processedData: "",
+        icon: null,
+        className: ""
       };
     case text === "visibility":
-      return { show: true, title: "Visibility", unit: "m", iconShow: true, icon: <></> };
+      return {
+        show: true,
+        title: "Visibility",
+        unit: "m",
+        processedData: "",
+        icon: null,
+        className: ""
+      };
     case text === "wind_speed":
-      return { show: true, title: "Wind speed", unit: "m/s", iconShow: true, icon: <></> };
+      return {
+        show: true,
+        title: "Wind speed",
+        unit: "m/s",
+        processedData: "",
+        icon: null,
+        className: ""
+      };
     case text === "wind_deg":
-      return { show: true, title: "Wind deg", unit: "\u{00B0}", iconShow: true, icon: <></> };
+      return {
+        show: true,
+        title: "Wind deg",
+        unit: "\u{00B0}",
+        processedData: "",
+        icon: null,
+        className: ""
+      };
     case text === "clouds_all":
-      return { show: true, title: "Clouds", unit: "\u{0025}", iconShow: true, icon: <></> };
+      return {
+        show: true,
+        title: "Clouds",
+        unit: "\u{0025}",
+        processedData: "",
+        icon: null,
+        className: ""
+      };
     case text === "dt":
-      return { show: false, title: "", unit: "", iconShow: true, icon: <></> };
+      return {
+        show: false,
+        title: "",
+        unit: "",
+        processedData: "",
+        icon: null,
+        className: ""
+      };
     case text === "sys_type":
-      return { show: false, title: "", unit: "", iconShow: true, icon: <></> };
+      return {
+        show: false,
+        title: "",
+        unit: "",
+        processedData: "",
+        icon: null,
+        className: ""
+      };
     case text === "sys_id":
-      return { show: false, title: "", unit: "", iconShow: true, icon: <></> };
+      return {
+        show: false,
+        title: "",
+        unit: "",
+        processedData: "",
+        icon: null,
+        className: ""
+      };
     case text === "sys_country":
-      return { show: true, title: "Country", unit: "", iconShow: true, icon: <></> };
+      return {
+        show: true,
+        title: "Country",
+        unit: "",
+        processedData: "",
+        icon: null,
+        className: ""
+      };
     case text === "sys_sunrise":
-      return { show: true, title: "Sunrise", unit: "", iconShow: true, icon: <></> };
+      return {
+        show: true,
+        title: "Sunrise",
+        unit: "",
+        processedData: `${typeof value === "number" && transformDate(value)}`,
+        icon: null,
+        className: ""
+      };
     case text === "sys_sunset":
-      return { show: true, title: "Sunset", unit: "", iconShow: true, icon: <></> };
+      return {
+        show: true,
+        title: "Sunset",
+        unit: "",
+        processedData: `${typeof value === "number" && transformDate(value)}`,
+        icon: null,
+        className: ""
+      };
     case text === "timezone":
-      return { show: true, title: "Timezone", unit: "", iconShow: true, icon: <></> };
+      return {
+        show: false,
+        title: "",
+        unit: "",
+        processedData: "",
+        icon: null,
+        className: ""
+      };
     case text === "id":
-      return { show: false, title: "", unit: "", iconShow: true, icon: <></> };
+      return {
+        show: false,
+        title: "",
+        unit: "",
+        processedData: "",
+        icon: null,
+        className: ""
+      };
     case text === "name":
-      return { show: true, title: "City", unit: "", iconShow: true, icon: <></> };
+      return {
+        show: true,
+        title: "City",
+        unit: "",
+        processedData: "",
+        icon: null,
+        className: ""
+      };
     case text === "cod":
-      return { show: false, title: "", unit: "", iconShow: true, icon: <></> };
+      return {
+        show: false,
+        title: "",
+        unit: "",
+        processedData: "",
+        icon: null,
+        className: ""
+      };
     default:
-      return { show: true, title: "", unit: "", iconShow: true, icon: <></> };
+      return { show: true, title: "", unit: "", processedData: "", icon: null };
   }
 }
 
