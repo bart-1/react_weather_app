6e9e9099052f889726b52adff7707766eb6d1e57
Bartosz Woldanski <studio@dziwnykot.pl>
2023-04-26 13:37:43 +0200

    refactor: new url for API server

--- src/hooks/useFetchApi.tsx
+++ src/hooks/useFetchApi.tsx
@@ -1,4 +1,4 @@
-import {  useEffect, useMemo, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { useQuery, useQueryClient } from "react-query";
 import {
   CurrentWeatherModel,
@@ -7,20 +7,21 @@ import {
 import { WeatherApiModel, emptyWeatherApi } from "../models/WeatherApiModel";
 import { WeatherBlocksProps } from "../WeatherBlocks";
 
-export const apiURL = `https://dev-1.pl/weather-api`;
+export const apiURL = `https://dziwnykot.pl/weather-api-second`;
 
 const useFetchApi = () => {
   const [city, setCity] = useState("");
   const [country, setCountry] = useState("");
   const [url, setUrl] = useState("");
-  const [weather, setWeather] = useState<CurrentWeatherModel>(emptyCurrentWeather);
+  const [weather, setWeather] =
+    useState<CurrentWeatherModel>(emptyCurrentWeather);
   const [timestamp, setTimestamp] = useState("");
   const queryClient = useQueryClient();
 
   const { data, status, refetch } = useQuery({
     enabled: !!url,
     retry: true,
-    queryKey: ["city", {"city": city}, "country", country, "weather", url],
+    queryKey: ["city", { city: city }, "country", country, "weather", url],
     queryFn: async (): Promise<WeatherApiModel> => {
       const response = await fetch(url).then((response) => {
         if (response.headers.get("content-type") === "text/html") {
@@ -30,9 +31,8 @@ const useFetchApi = () => {
           return emptyWeather;
         } else if (response.headers.get("content-type") === "application/json")
           return response.json();
-      }); 
-    
- 
+      });
+
       return response;
     },
   });
@@ -42,9 +42,8 @@ const useFetchApi = () => {
   }, []);
 
   useEffect(() => {
-    if (url)
-      refetch()
-      queryClient.clear();
+    if (url) refetch();
+    queryClient.clear();
   }, [url, city, country]);
 
   useEffect(() => {
@@ -52,20 +51,20 @@ const useFetchApi = () => {
       setWeather(JSON.parse(data.weather));
       setTimestamp(data.updated_at);
     }
-
   }, [status]);
 
-
-  const setQuery = ({countryCode, city}:WeatherBlocksProps) => {
-
-    setUrl(`${apiURL}/${countryCode}/${city}`)
-    setCity(city)
-    setCountry(countryCode)
+  const setQuery = ({ countryCode, city }: WeatherBlocksProps) => {
+    setUrl(`${apiURL}/${countryCode}/${city}`);
+    setCity(city);
+    setCountry(countryCode);
   };
 
-  return { status: status, weather: weather, timestamp: timestamp, setQuery: setQuery}
-      
-}
-
+  return {
+    status: status,
+    weather: weather,
+    timestamp: timestamp,
+    setQuery: setQuery,
+  };
+};
 
 export default useFetchApi;
